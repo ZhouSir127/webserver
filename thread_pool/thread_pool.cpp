a2255c1700dcd177c13e13bf9d9719d2da42b473
root <root@example.com>
2026-04-27 16:40:15 +0800

    4.2.16:40

--- thread_pool/thread_pool.cpp
+++ thread_pool/thread_pool.cpp
@@ -28,7 +28,7 @@ void ThreadPool::run()
                 case HttpCode::NO_RESOURCE:
                 case HttpCode::FORBIDDEN_REQUEST:
                 case HttpCode::FILE_REQUEST:
-                    EpollManager::getInstance().modify(conn->getChannel(), ::EPOLLOUT);
+                    EpollManager::getInstance().modify(conn->getChannel(), ::EPOLLOUT | EPOLLRDHUP | EPOLLONESHOT | (httpManager.getConnectEt() ? EPOLLET : static_cast<uint32_t>(0)) );
                     timerManager.adjust(work.first);
                     break;
                 default: // 满足 -Wswitch-default，处理预料之外的情况
@@ -38,10 +38,10 @@ void ThreadPool::run()
             HttpCode ret = conn->write();
     
             if ( ret == HttpCode::NO_REQUEST ){//继续写
-                EpollManager::getInstance().modify(conn->getChannel(), EPOLLOUT);
+                EpollManager::getInstance().modify(conn->getChannel(), EPOLLOUT | EPOLLRDHUP | EPOLLONESHOT | (httpManager.getConnectEt() ? EPOLLET : static_cast<uint32_t>(0)) );
                 timerManager.adjust(work.first);
             }else if ( ret == HttpCode::GET_REQUEST && conn->getLinger() ){
-                EpollManager::getInstance() .modify(conn->getChannel(), EPOLLIN);
+                EpollManager::getInstance() .modify(conn->getChannel(), EPOLLIN | EPOLLRDHUP | EPOLLONESHOT | (httpManager.getConnectEt() ? EPOLLET : static_cast<uint32_t>(0)) );
                 timerManager.adjust(work.first);
                 conn->init();
             }else{
