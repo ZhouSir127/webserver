ca4c7c8c77f0c743a1ef8b0f0f5543dd3005dd3f
root <root@example.com>
2026-05-09 20:20:47 +0800

    5.9:20:20

--- crypto_util/crypto_util.d
+++
@@ -1,3 +0,0 @@
-crypto_util/crypto_util.o: crypto_util/crypto_util.cpp \
- crypto_util/crypto_util.h
-crypto_util/crypto_util.h:
--- log/log.d
+++
@@ -1,6 +0,0 @@
-log/log.o: log/log.cpp log/log.h log/../args.h \
- log/../work_queue/work_queue.h log/../work_queue/../log/log.h
-log/log.h:
-log/../args.h:
-log/../work_queue/work_queue.h:
-log/../work_queue/../log/log.h:
